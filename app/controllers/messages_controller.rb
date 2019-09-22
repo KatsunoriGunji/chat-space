f6724d044bf3f16b2977be56a8ec21972c6c54a6
KatsunoriGunji <l.hvokj@gmail.com>
2019-09-22 17:09:19 +0900

    implement the comment post function with Ajax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,18 +7,24 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
-    else
-      @messages = @group.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください。'
-      render :index
+    respond_to do |format|
+      @message.save
+      format.html {redirect_to group_messages_path(@group)}
+      format.json
     end
+
+    # if @message.save
+    #   redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+    # else
+    #   @messages = @group.messages.includes(:user)
+    #   flash.now[:alert] = 'メッセージを入力してください。'
+    #   render :index
+    # end
   end
 
   private
   def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id, user_name: current_user.name)
   end
 
   def set_group
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
+json.id @message.id
 json.content @message.content
-json.image @message.image
-json.group_id @message.group_id
-json.user_id @message.user_id
-json.created_at @message.created_at
+json.image @message.image.present? ? @message.image.url : ""
+json.user_name @message.user_name
+json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
